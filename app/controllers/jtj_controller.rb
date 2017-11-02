82b2a81fb97ca3eeb2f709d7efd040a5006a763f
rajesh <mcpuram@gmail.com>
2017-11-02 20:06:24 +0530

    latest code in jit

--- app/controllers/jtj_controller.rb
+++ app/controllers/jtj_controller.rb
@@ -1,51 +1,19 @@
 require 'net/http'
+require 'json'
 
 class JtjController < ApplicationController
 
-  	def index
-  		
-  		url = URI("#{APP_CONFIG[:watermarkchurchURL]}/")
-  	    http = Net::HTTP.new(url.host, url.port)
-		http.use_ssl = true
-		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-		request = Net::HTTP::Get.new(url)
-		request["content-type"] = 'application/json'
-		response = http.request(request)
-		@data = response.read_body
-		@val=JSON.parse(@data)
-
-		if @val.present?
-			
-			curriculumId=@val["curriculum"]["id"]
-			url1 = URI("#{APP_CONFIG[:watermarkchurchURL]}/curriculums/#{curriculumId}/entries")
-			http1 = Net::HTTP.new(url1.host, url1.port)
-			http1.use_ssl = true
-			http1.verify_mode = OpenSSL::SSL::VERIFY_NONE
-			request1 = Net::HTTP::Get.new(url1)
-			request1["content-type"] = 'application/json'
-			response1 = http1.request(request1).read_body
-			@entityData=JSON.parse(response1)
-			# binding.pry
-			pp @entityData
-
-			@entityResponse=[];
-			
-			if @entityData.present?	
-				@entityData["entries"].each do |key|
-					entityId=key["id"];
-					url = URI("#{APP_CONFIG[:watermarkchurchURL]}/entries/#{entityId}")
-					http = Net::HTTP.new(url.host, url.port)
-					http.use_ssl = true
-					http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-					request = Net::HTTP::Get.new(url)
-					request["content-type"] = 'application/json'
-					response = http.request(request)
-					@data = response.read_body
-					@entityResponse << JSON.parse(@data)
-				end
-			end
-
+	def index
+	 	begin
+			url = "#{APP_CONFIG[:watermarkchurchURL]}/"
+			uri = URI(url)
+			response = Net::HTTP.get(uri)
+			@val = JSON.parse(response)
+	  	rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
+	       Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
+	           render json: {
+	           	error: e.to_s
+	           	}, status: :not_found
 		end
-		pp @entityResponse
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-   	get 'jtj/index'
+   	# get 'jtj/index'
   	root 'jtj#index'
   	resources :jtj, only: [:index]
   	get '/auth/provider/callback', to: 'sessions#create' #OAUTH Call Back URL
