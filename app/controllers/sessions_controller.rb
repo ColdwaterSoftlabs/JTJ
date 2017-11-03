ca345c2bc05e24dc538f18f5cc4eb9771655a893
rajesh <mcpuram@gmail.com>
2017-11-03 22:19:08 +0530

    oauth call back url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
 	def create
   		@auth = {}
   		if params[:code].nil? and @auth.nil? 
-	  		
+	  	
 	  	else
 	  		@auth["code"]=params[:code]
 	  		@auth["state"]=params[:state]
-			redirect_to '/' and return
+	  		redirect_to '/' and return
 	  	end
 	  	pp @auth
 	end
