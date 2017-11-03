a13730692e01890d9b2a43b8cac01d43077ff9cb
rajesh <mcpuram@gmail.com>
2017-11-03 22:13:19 +0530

    OAUth call back url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,13 @@
 class SessionsController < ApplicationController
 	def create
-  		# return p "OAuth"
+  		@auth = {}
+  		if params[:code].nil? and @auth.nil? 
+	  		
+	  	else
+	  		@auth["code"]=params[:code]
+	  		@auth["state"]=params[:state]
+			redirect_to '/' and return
+	  	end
+	  	pp @auth
 	end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
    	# get 'jtj/index'
   	root 'jtj#index'
   	resources :jtj, only: [:index]
-  	get '/auth/provider/callback', to: 'sessions#create' #OAUTH Call Back URL
+  	get '/callback', to: 'sessions#create' #OAUTH Call Back URL
 end
