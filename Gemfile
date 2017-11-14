285630abbcd6323428bd6228f21162ad91682362
Vigneshwaran Sivalingam <vigneshsika@gmail.com>
2017-11-15 01:19:53 +0530

    Add discourse comments fetch and display

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'sendgrid-ruby'
 gem 'whenever', :require => false
 gem 'delayed_job_active_record'
 
+gem 'rest-client'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/jtj_controller.rb
+++ app/controllers/jtj_controller.rb
@@ -2,6 +2,7 @@ require 'net/http'
 require 'json'
 
 class JtjController < ApplicationController
+	before_action :set_discourse_credentials
 
 	def index
 		@todayNews={}
@@ -18,8 +19,31 @@ class JtjController < ApplicationController
 		end
 		if @todayNews.nil?
 		else
-			@todayNews=@val["latest_entry"]["scripture"]["html"];
-			UserEmailNotificationMailer.send_email_notify(@todayNews).deliver
+			# @todayNews=@val["latest_entry"]["scripture"]["html"];
+			# UserEmailNotificationMailer.send_email_notify(@todayNews).deliver
 		end
+		response = fetch_discource_topic 14
+		if response.code == 200
+			content = JSON.parse(response.body)
+			posts_count = content["posts_count"]
+			@posts = content["post_stream"]["posts"]
+			p @posts
+		else
+			p "error fetching topic"
+		end
+	end
+
+	private
+
+	def set_discourse_credentials
+		@url = "http://discourse.coldwatersl.in"
+		@api_key = "69c1ba0560a33bd00d3914edb542aa9658b2cbefffe28ab56f7207274d141f13"
+		@api_username = "daniel"
+	end
+
+	def fetch_discource_topic topic_id 
+		url = "#{@url}/t/#{topic_id}.json?api_key=#{@api_key}&api_username=#{@api_username}"
+		p url
+		response = RestClient.get(url)
 	end
 end
