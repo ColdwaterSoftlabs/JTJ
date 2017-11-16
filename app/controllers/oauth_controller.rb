584ff0ca93783b2c0bedc1f9b3d914e3eb7852ba
rajesh <mcpuram@gmail.com>
2017-11-16 12:09:26 +0530

    remove unwated file.

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -3,7 +3,7 @@ class OauthController < ApplicationController
 	def oauth_create
 		client = OAuth2::Client.new("#{APP_CONFIG[:JTJ_APPLICATION_ID]}", "#{APP_CONFIG[:JTJ_APPLICATION_SECRET_KEY]}", :site => "#{APP_CONFIG[:OAUTH_BASE_URL]}")
 		client.auth_code.authorize_url(:redirect_uri => "#{APP_CONFIG[:OAUTH_CALL_BACK_URL]}")
-		token = client.client_credentials.
+		token = client.client_credentials.get_token
 		render json: token 
 	end
 end
--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,11 @@ module Watermarker
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-    config.active_job.queue_adapter = :delayed_job
+    # config.active_job.queue_adapter = :delayed_job
+    config.api_only = true
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
