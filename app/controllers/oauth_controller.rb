13c6612ffc250b7c2c749b588b336defe49fb3d8
Vigneshwaran Sivalingam <vigneshsika@gmail.com>
2017-11-18 18:52:37 +0530

    Fix syntax error in oauth controller

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,7 +1,7 @@
 require 'oauth2'
 class OauthController < ApplicationController
 	def oauth_create
-		client = OAuth2::Client.new(ENV['JTJ_APPLICATION_ID'], ENV['JTJ_APPLICATION_SECRET_KEY'], :site => ENV['WATERMARK_BASE_URL')
+		client = OAuth2::Client.new(ENV['JTJ_APPLICATION_ID'], ENV['JTJ_APPLICATION_SECRET_KEY'], :site => ENV['WATERMARK_BASE_URL'])
 		client.auth_code.authorize_url(:redirect_uri => "https://jtj-dev.herokuapp.com/callback")
 		token = client.client_credentials.get_token
 		User.create token:token
