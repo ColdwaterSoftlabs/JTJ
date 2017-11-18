5b51c13744cf65dcb9ce93fcfd485b0f18e0e278
Vigneshwaran Sivalingam <vigneshsika@gmail.com>
2017-11-18 21:13:13 +0530

    Add save access_token alone

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -4,7 +4,7 @@ class OauthController < ApplicationController
 		client = OAuth2::Client.new(ENV['JTJ_APPLICATION_ID'], ENV['JTJ_APPLICATION_SECRET_KEY'], :site => ENV['WATERMARK_BASE_URL'])
 		client.auth_code.authorize_url(:redirect_uri => "https://jtj-dev.herokuapp.com/callback")
 		token = client.client_credentials.get_token
-		User.create token:token
+		User.create token:token['access_token']
 		p 'token created'
 		render json: token 
 	end
