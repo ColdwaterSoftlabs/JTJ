b4edca7dd16ac049633261dc9f08be9be030e00b
Vigneshwaran Sivalingam <vigneshsika@gmail.com>
2017-11-22 00:18:45 +0530

    Add render for oauth create

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,5 +1,6 @@
 require 'oauth2'
 class OauthController < ApplicationController
+	before_action :set_discourse_credentials
 
 	def oauth_create
 		client_id = ENV['JTJ_APPLICATION_ID']
@@ -8,6 +9,7 @@ class OauthController < ApplicationController
 		redirect_uri = ENV['REDIRECT_URI']
 		client = OAuth2::Client.new(client_id, client_secret, :site => site)
 		client.auth_code.authorize_url(:redirect_uri => redirect_uri)
+		render json: "success"
 	end
 
 	
