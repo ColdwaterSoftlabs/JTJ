6d776b94810f4fdfecf032d67e3e3ca744ba7bbd
Vigneshwaran Sivalingam <vigneshsika@gmail.com>
2017-11-22 00:11:12 +0530

    Fix oauth

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -3,58 +3,13 @@ class OauthController < ApplicationController
 	before_action :set_discourse_credentials
 
 	def oauth_create
-		client = OAuth2::Client.new(ENV['JTJ_APPLICATION_ID'], ENV['JTJ_APPLICATION_SECRET_KEY'], :site => ENV['WATERMARK_BASE_URL'])
-		client.auth_code.authorize_url(:redirect_uri => "https://jtj-dev.herokuapp.com/callback")
-		token = client.client_credentials.get_token
-		# p token
-		response = token.get('/api/v1/me.json')
-		p response
-		# get_user_details token['access_token']
-		# get_user_details "accesstokenabcedefk"
-		p 'token created'
-		render json: token
-		# redirect_to root_path 
+		client_id = ENV['JTJ_APPLICATION_ID']
+		client_secret = ENV['JTJ_APPLICATION_SECRET_KEY']
+		site = ENV['WATERMARK_BASE_URL']
+		redirect_uri = ENV['REDIRECT_URI']
+		client = OAuth2::Client.new(client_id, client_secret, :site => site)
+		client.auth_code.authorize_url(:redirect_uri => redirect_uri)
 	end
 
-	private
-
-	def get_user_details token
-		# Have to fetch real user email
-		username = "jtj#{rand(252...4350)}"
-		email = "#{username}@jtj.com"
-		create_user email, username, token
-	end
-
-	def create_user email, username, token
-		generated_password = Devise.friendly_token.first(8)
-		discourse_email = email #should be changed to dummy email, not from the user details email
-		@user = User.create!(email: email, password: generated_password, password_confirmation: generated_password, username: username, wcc_access_token: token, discourse_email: discourse_email)
-		p "User created Successfully"
-		sign_in(@user)
-		p "User signed in Succesfully"
-		create_discourse_user
-	end
-
-	def create_discourse_user
-		user = @client.create_user(
-			name: @user.username,
-			email: @user.discourse_email,
-			username: @user.username,
-			password: "#{@user.discourse_email.reverse}"
-		)
-		p "----------Discourse user details--------------------"
-		p user
-		activate_discourse_user user
-	end 
-
-	def set_discourse_credentials
-		@client = DiscourseApi::Client.new(ENV["DISCOURSE_URL"])
-		@client.api_key = ENV["DISCOURSE_API_KEY"]
-		@client.api_username = ENV["DISCOURSE_API_USERNAME"]
-	end
-
-	def activate_discourse_user user
-		p "Discourse user activated"
-		@client.activate(user["user_id"])
-	end
+	
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,64 @@ class SessionsController < ApplicationController
 	def create
   		@auth = {}
   		if params[:code].nil? and @auth.nil? 
-	  	
+	  		p "Some error occured, no code"
 	  	else
+	  		p "Printing params"
+	  		p params
 	  		@auth["code"]=params[:code]
 	  		@auth["state"]=params[:state]
+	  		token = client.auth_code.get_token(@auth['code'], :redirect_uri => redirect_uri)
+	  		#store access_token and refresh token
+	  		get_user_details token
 	  		redirect_to '/' and return
 	  	end
-	  	pp @auth
+	  	p @auth
+	end
+
+	private
+
+	def get_user_details token
+		# Have to fetch real user email
+		# username = "jtj#{rand(252...4350)}"
+		# email = "#{username}@jtj.com"
+		response = token.get('/api/v1/me.json')
+	  	user = JSON.parse(response.body)
+	  	email = user["email"]
+	  	first_name = user["first_name"]
+		create_user email, first_name
+	end
+
+	def create_user email, username
+		generated_password = Devise.friendly_token.first(8)
+		discourse_email = "#{username}@jtj.com" #should be changed to dummy email, not from the user details email
+		@user = User.create!(email: email, password: generated_password, password_confirmation: generated_password, username: username, discourse_email: discourse_email)
+		p "User created Successfully"
+		sign_in(@user)
+		p "User signed in Succesfully"
+		create_discourse_user
+	end
+
+	def create_discourse_user
+		set_discourse_credentials
+		user = @client.create_user(
+			name: @user.username,
+			email: @user.discourse_email,
+			username: @user.username,
+			password: "#{@user.discourse_email.reverse}"
+		)
+		p "----------Discourse user details--------------------"
+		p user
+		activate_discourse_user user
+	end 
+
+	def set_discourse_credentials
+		@client = DiscourseApi::Client.new(ENV["DISCOURSE_URL"])
+		@client.api_key = ENV["DISCOURSE_API_KEY"]
+		@client.api_username = ENV["DISCOURSE_API_USERNAME"]
+	end
+
+	def activate_discourse_user user
+		p "Discourse user activated"
+		@client.activate(user["user_id"])
 	end
 end
